0773b9064b68552bd119f0f4e3a319472768357d
alezingamedev <86687900+alezingamedev@users.noreply.github.com>
2021-10-28 09:12:36 -0300

    Create programaCalcPM.py

---
+++ programaCalcPM.py
@@ -0,0 +1,42 @@
+import time
+import random
+print ("Resolvedor de Principio da multiplicação by ale")
+print ("se você não for usar algum fator, use o 1 pra anular o fator")
+print ("calculadora faz mais, porém não é comica igual esse programa.")
+time.sleep(1)
+var1=int(input("qual o primeiro fator?"))
+var2=int(input("qual o segundo fator?"))
+var3=int(input("qual o terceiro fator?"))
+var4=int(input("qual o quarto fator?"))
+var5=int(input("qual o quinto fator?"))
+var6=int(input("qual o sexto fator?"))
+varfinal = var1 * var2 * var3 * var4 * var5 * var6
+print ("calculando resposta.")
+if varfinal>10000:
+  print ("obtendo mais nucleos para processamento de resposta, aguarde.")
+  time.sleep(20)
+  print ("obtido")
+time.sleep(5)
+print ("calculando resposta...")
+print ("obtendo vetores finais para inicialização de resposta quantica absoluta.")
+print ("propaganda desnecessaria:Feliz Halloween ")
+time.sleep(1)
+print ("obtido.")
+time.sleep(1)
+print ("resposta abaixo:")
+print (varfinal)
+
+varsurp=random.randint(1,50)
+
+if varsurp==1:
+  print ("você  achou um ovo de pascoa enquanto o programa calculava a hash.")
+if varsurp==3:
+  print ("enquanto calculava você achou um genio da lampada, que lhe deu uma abobora de halloween iluminada, o famoso jack o'lantern e docês")
+if varsurp==10:
+  print ("o programa chegou ao papai noel e ele deu um presente enquanto calculava.")
+if varsurp==40:
+  print ("o programa encontrou uma falha no espaço-tempo durante o calculo dos vetores.")
+if varsurp==24:
+  print ("uma falha da matrix, deseja sair dessa matrix usando a falha?")
+  time.sleep(3)
+  print ("tarde demais, a falha foi corrigida.")
